6186ad4a5b6433053000ff7bac147464268bd24f
12eu8oed <150359214+12eu8oed@users.noreply.github.com>
2024-01-05 00:47:34 +0900

    테이블 생성 쿼리
    메인 테이블이랑 조회수 테이블

---
+++ tbl_board 메인테이블.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `tbl_board` (
+	`bno` INT(11) NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(50) NOT NULL COLLATE 'utf8mb3_general_ci',
+	`content` TEXT NOT NULL COLLATE 'utf8mb3_general_ci',
+	`writer` VARCHAR(30) NOT NULL COLLATE 'utf8mb3_general_ci',
+	`regDate` TIMESTAMP NOT NULL DEFAULT current_timestamp(),
+	`viewCnt` INT(11) NULL DEFAULT '0',
+	PRIMARY KEY (`bno`) USING BTREE
+)
+COLLATE='utf8mb3_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=1722
+;
