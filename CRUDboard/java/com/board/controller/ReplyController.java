c89fea28ed2f5bc64d6e6a6eb179d089ff4a92fb
12eu8oed <150359214+12eu8oed@users.noreply.github.com>
2024-01-11 01:10:22 +0900

    댓글 작성까지 구현완료 (댓글 달린 게시글 삭제하면, 외래 키 제약조건으로 실패 해결해야함)

--- CRUDboard/java/com/board/controller/ReplyController.java
+++ CRUDboard/java/com/board/controller/ReplyController.java
@@ -15,19 +15,22 @@ public class ReplyController {
 
 	@Inject
 	private ReplyService replyService; // 서비스 객체 주입
-
+	
 	// 댓글 조회
+	
 	// 댓글 작성
 	@RequestMapping(value = "write", method = RequestMethod.POST)
 	public String postWrite(ReplyVO vo) throws Exception {
-
+		
 		replyService.write(vo);
 
-		// 댓글을 작성했던 게시물로 다시 이동 / 동작순서 : 사용자(클라이언트) => Controller => Service => DAO=> Mapper => DB 그후 조회중이였던 게시물로 이동
-		return "redirect:/board/view?bno=" + vo.getBno(); 
+		// 댓글을 작성했던 게시물로 다시 이동 
+		// 동작순서 : 사용자가요청 => view.jsp에서 댓글 작성 => Controller => Service => DAO=> Mapper => DB 그후 조회중이였던 게시물로 이동
+		return "redirect:/board/view?bno=" + vo.getBno();
 
 	}
 	// 댓글 수정
+	
 	// 댓글 삭제
 
 }
