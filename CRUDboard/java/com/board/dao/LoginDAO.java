d9e7eb6891e6c6febf5ea6a900a0b96bdffddc16
12eu8oed <150359214+12eu8oed@users.noreply.github.com>
2024-01-19 22:33:16 +0900

    commit

--- CRUDboard/java/com/board/dao/LoginDAO.java
+++
@@ -1,9 +0,0 @@
-package com.board.dao;
-
-import java.util.List;
-
-import com.board.domain.BoardVO;
-
-public interface LoginDAO {
-	// 로그인
-}
--- CRUDboard/java/com/board/dao/SignUpDAO.java
+++
@@ -1,9 +0,0 @@
-package com.board.dao;
-
-import java.util.List;
-
-import com.board.domain.BoardVO;
-
-public interface SignUpDAO {
-	// 로그인
-}
